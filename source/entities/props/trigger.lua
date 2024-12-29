15c16fed9500c8d214e1b8a11548a1d61d400c83
dactrtr <dactrtr@gmail.com>
2024-12-29 16:31:09 +0900

    fixed trigger position

--- source/entities/props/trigger.lua
+++ source/entities/props/trigger.lua
@@ -7,10 +7,11 @@ function Trigger:init(x, y, width, height, script, position, room, type)
   self.room = room
   self.type = type
   self:setCollideRect(0, 0, width,height)
+  --self:setCenter(0.5, 0.5)
   self:setZIndex(3)
-  self:moveTo(x, y)
+  self:moveTo(x-width/2, y-height/2)
   self:setGroups(3)
-  self:add(x,y)
+  self:add()
 end
 
 function Trigger:returnScript()
