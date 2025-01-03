26187858dba7b5be3a7787102d9ff4aec02fca88
dactrtr <dactrtr@gmail.com>
2025-01-03 19:59:42 +0900

    prettier format

--- source/assets/data/levels.lua
+++ source/assets/data/levels.lua
@@ -8,126 +8,125 @@ levels = {
 			light = 0.5,
 			shadow = false,
 			doors = {
-			{
-				direction = 'top',
-				open = 'open',
-				leadsTo = 102
-			}
+				{
+					direction = 'top',
+					open = 'open',
+					leadsTo = 102
+				}
 			},
 			comic = {
-					wasPlayed = false,
-					name = "intro-comic",
-					play = "enter"
+				wasPlayed = false,
+				name = "intro-comic",
+				play = "enter"
 			},
 			items = {},
 			triggers = {
-			{
-				usedTrigger = false,
-				x = 263,
-				y = 144,
-				width = 38,
-				height = 40,
-				script = "bigGiftForcrew233"
+				{
+					usedTrigger = false,
+					x = 263,
+					y = 144,
+					width = 38,
+					height = 40,
+					script = "bigGiftForcrew233"
+				},
+				{
+					usedTrigger = false,
+					x = 316,
+					y = 179,
+					width = 38,
+					height = 30,
+					script = "bigGiftforCrew488"
+				},
+				{
+					usedTrigger = false,
+					x = 32,
+					y = 43,
+					width = 45,
+					height = 60,
+					script = "momMicrowave"
+				},
+				{
+					usedTrigger = false,
+					x = 200,
+					y = 151,
+					width = 80,
+					height = 37,
+					script = "wakeup"
+				}
 			},
-			{
-				usedTrigger = false,
-				x = 316,
-				y = 179,
-				width = 38,
-				height = 30,
-				script = "bigGiftforCrew488"
-			},
-			{
-				usedTrigger = false,
-				x = 32,
-				y = 43,
-				width = 45,
-				height = 60,
-				script = "momMicrowave"
-			},
-			{
-				usedTrigger = false,
-				x = 200,
-				y = 151,
-				width = 80,
-				height = 37,
-				script = "wakeup"
-			}
-			},
-			enemies = {
-			},
+			enemies = {},
 			props = {
-			{
-				type = "fellchair",
-				x = 35,
-				y = 185
-			},
-			{
-				type = "fellchair",
-				x = 78,
-				y = 163
-			},
-			{
-				type = "chair",
-				x = 162,
-				y = 202
-			},
-			{
-				type = "gift",
-				x = 269,
-				y = 143
-			},
-			{
-				type = "gifts",
-				x = 348,
-				y = 157
-			},
-			{
-				type = "gift",
-				x = 317,
-				y = 172
-			},
-			{
-				type = "xtree-1",
-				x = 301,
-				y = 87
-			},
-			{
-				type = "xtree-2",
-				x = 333,
-				y = 88
-			},
-			{
-				type = "xtree-3",
-				x = 302,
-				y = 118
-			},
-			{
-				type = "xtree-4",
-				x = 334,
-				y = 118
-			},
-			{
-				type = "fellTable",
-				x = 238,
-				y = 206
-			},
-			{
-				type = "gifts",
-				x = 287,
-				y = 155
-			},
-			{
-				type = "smallTable",
-				x = 32,
-				y = 49
-			},
-			{
-				type = "microwave",
-				x = 33,
-				y = 31
-			},
+				{
+					type = "fellchair",
+					x = 35,
+					y = 185
+				},
+				{
+					type = "fellchair",
+					x = 78,
+					y = 163
+				},
+				{
+					type = "chair",
+					x = 162,
+					y = 202
+				},
+				{
+					type = "gift",
+					x = 269,
+					y = 143
+				},
+				{
+					type = "gifts",
+					x = 348,
+					y = 157
+				},
+				{
+					type = "gift",
+					x = 317,
+					y = 172
+				},
+				{
+					type = "xtree-1",
+					x = 301,
+					y = 87
+				},
+				{
+					type = "xtree-2",
+					x = 333,
+					y = 88
+				},
+				{
+					type = "xtree-3",
+					x = 302,
+					y = 118
+				},
+				{
+					type = "xtree-4",
+					x = 334,
+					y = 118
+				},
+				{
+					type = "fellTable",
+					x = 238,
+					y = 206
+				},
+				{
+					type = "gifts",
+					x = 287,
+					y = 155
+				},
+				{
+					type = "smallTable",
+					x = 32,
+					y = 49
+				},
+				{
+					type = "microwave",
+					x = 33,
+					y = 31
+				}
 			}
-		},
-	},
+		}
+	}
 }
--- source/assets/data/script.lua
+++ source/assets/data/script.lua
@@ -1,61 +1,55 @@
 script = {
-	{
-		name = "wakeup",
-		dialog = {
-			{
-		video = 'player',
-		text = Graphics.getLocalizedText("wakeup-01", "en"),
-	},
-					{
-		video = 'playerWorry',
-		text = Graphics.getLocalizedText("wakeup-02", "en"),
-	}
-			
-		}
-	},
-	{
-		name = "momMicrowave",
-		dialog = {
-			{
-		video = 'playerWorry',
-		text = Graphics.getLocalizedText("mommicrowave-01", "en"),
-		
-	},
-					{
-		video = 'player',
-		text = Graphics.getLocalizedText("mommicrowave-02", "en"),
-		screen = Graphics.image.new('assets/images/ui/dialog/img/spaceship.png')
-	}
-			
-		}
-	},
-	{
-		name = "bigGiftForcrew233",
-		dialog = {
-			{
-		video = 'playerSurprise',
-		text = Graphics.getLocalizedText("biggiftforcrew233-01", "en"),
-	},
-					{
-		video = 'playerHappy',
-		text = Graphics.getLocalizedText("biggiftforcrew233-02", "en"),
-	},
-					{
-		video = 'playerWorry',
-		text = Graphics.getLocalizedText("biggiftforcrew233-03", "en"),
-	}
-			
-		}
-	},
-	{
-		name = "bigGiftforCrew488",
-		dialog = {
-			{
-		video = 'playerWorry',
-		text = Graphics.getLocalizedText("biggiftforcrew488-01", "en"),
-	}
-			
-		}
-	},
-	
+    {
+        name = "wakeup",
+        dialog = {
+            {
+                video = 'player',
+                text = Graphics.getLocalizedText("wakeup-01", "en")
+            },
+            {
+                video = 'playerWorry',
+                text = Graphics.getLocalizedText("wakeup-02", "en")
+            }
+        }
+    },
+    {
+        name = "momMicrowave",
+        dialog = {
+            {
+                video = 'playerWorry',
+                text = Graphics.getLocalizedText("mommicrowave-01", "en")
+            },
+            {
+                video = 'player',
+                text = Graphics.getLocalizedText("mommicrowave-02", "en"),
+                screen = Graphics.image.new('assets/images/ui/dialog/img/spaceship.png')
+            }
+        }
+    },
+    {
+        name = "bigGiftForcrew233",
+        dialog = {
+            {
+                video = 'playerSurprise',
+                text = Graphics.getLocalizedText("biggiftforcrew233-01", "en")
+            },
+            {
+                video = 'playerHappy',
+                text = Graphics.getLocalizedText("biggiftforcrew233-02", "en")
+            },
+            {
+                video = 'playerWorry',
+                text = Graphics.getLocalizedText("biggiftforcrew233-03", "en")
+            }
+        }
+    },
+    {
+        name = "bigGiftforCrew488",
+        dialog = {
+            {
+                video = 'playerWorry',
+                text = Graphics.getLocalizedText("biggiftforcrew488-01", "en")
+            }
+        }
+    }
 }
