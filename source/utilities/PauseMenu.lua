d45f63c7a1ed0ad86c1f7fd4b2621155376c11ed
dactrtr <dactrtr@gmail.com>
2025-01-06 11:24:44 +0900

    fixed mapfilling error on empty levels

--- source/utilities/PauseMenu.lua
+++ source/utilities/PauseMenu.lua
@@ -71,33 +71,35 @@ function mapFillingAndChecking()
 			posY = 26
 			
 		-- mark the position where the player actually is
-		if levels[i].floor.visited == true and PlayerData.actualLevel == levels[i].floor.level and PlayerData.actualRoom == levels[i].floor.roomNumber and i == levels[i].floor.roomNumber then
-			col = (i - 1) % 5 --(01234)
-			row = math.floor((i - 1) / 5) --(012)
+		if levels[i] ~= nil then
+			if levels[i].floor.visited == true and PlayerData.actualLevel == levels[i].floor.level and PlayerData.actualRoom == levels[i].floor.roomNumber and i == levels[i].floor.roomNumber then
+				col = (i - 1) % 5 --(01234)
+				row = math.floor((i - 1) / 5) --(012)
+				
+				Graphics.pushContext(menuImg)
+				
+				Graphics.setColor(Graphics.kColorWhite)
+				Graphics.fillRect(posX + 1 + (col * 6), posY + 1 + (row * 6), 5, 5)
+				
+				Graphics.setColor(Graphics.kColorBlack)
+				
+				Graphics.fillRect(posX + 2 + (col * 6), posY + 2 + (row * 6), 3, 3)
+				
+				Graphics.popContext() 
+				
+			-- clear the fog of a previously visited room		
+			elseif levels[i].floor.visited == true and i == levels[i].floor.roomNumber then
+				
+				col = (i - 1) % 5
+				row = math.floor((i - 1) / 5)
+				
+				Graphics.pushContext(menuImg)
+				Graphics.setColor(Graphics.kColorWhite)
+				Graphics.fillRect(posX + 1 + (col * 6), posY + 1 + (row * 6), 5, 5)
+				
+				Graphics.popContext() 
 			
-			Graphics.pushContext(menuImg)
-			
-			Graphics.setColor(Graphics.kColorWhite)
-			Graphics.fillRect(posX + 1 + (col * 6), posY + 1 + (row * 6), 5, 5)
-			
-			Graphics.setColor(Graphics.kColorBlack)
-			
-			Graphics.fillRect(posX + 2 + (col * 6), posY + 2 + (row * 6), 3, 3)
-			
-			Graphics.popContext() 
-			
-		-- clear the fog of a previously visited room		
-		elseif levels[i].floor.visited == true and i == levels[i].floor.roomNumber then
-			
-			col = (i - 1) % 5
-			row = math.floor((i - 1) / 5)
-			
-			Graphics.pushContext(menuImg)
-			Graphics.setColor(Graphics.kColorWhite)
-			Graphics.fillRect(posX + 1 + (col * 6), posY + 1 + (row * 6), 5, 5)
-			
-			Graphics.popContext() 
-		
+			end
 		end
 	
 	end
