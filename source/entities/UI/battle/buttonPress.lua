da6d275f325e8dbe75100e0e2758f50fcd9488f5
dactrtr <dactrtr@gmail.com>
2025-04-12 21:37:13 +0900

    time to do some UI, collisions almost fixed

--- source/entities/UI/battle/buttonPress.lua
+++ source/entities/UI/battle/buttonPress.lua
@@ -3,7 +3,7 @@ class('ButtonPress').extends(NobleSprite)
 
 local BUTTON_KEYS = { "aButton", "bButton", "leftButton", "upButton", "rightButton", "downButton", "Break" }
 
-function ButtonPress:init(beats)
+function ButtonPress:init(beats,startPoint)
 	ButtonPress.super.init(self, 'assets/images/ui/battle/button', true)
 
 	for i, name in ipairs(BUTTON_KEYS) do
@@ -14,11 +14,12 @@ function ButtonPress:init(beats)
 	self.animation.empty.frameDuration = 6
 	
 	self.bpm = beats
+	self.startPoint = startPoint
 	self.active = false
 	self.range = 100
 	self:setSize(32, 32)
 	self:setCollideRect(0, 0, 32, 32)
-	self:add(250, 30)
+	self:add(startPoint, 30)
 
 	self.buttonKey = ButtonPress.getRandomButtonKey()
 	self.animation:setState(self.buttonKey)
@@ -27,7 +28,8 @@ end
 function ButtonPress:hit()
 	self.buttonKey = "empty"
 	self.animation:setState(self.buttonKey)
-	self:tryMoveToFreePosition()
+	-- self:tryMoveToFreePosition()
+	self:moveTo(self.startPoint, self.y)
 	self:changeButtonSprite()
 end
 
@@ -41,54 +43,18 @@ function ButtonPress:changeButtonSprite()
 	self.animation:setState(self.buttonKey)
 end
 
-function ButtonPress:tryMoveToFreePosition()
-	local goalX = 330
-	local goalY = self.y
-
-	local _, _, collisions, count = self:checkCollisions(goalX, goalY)
-
-	local collisionWithButtonPress = false
-	for i = 1, count do
-		if collisions[i].other:isa(ButtonPress) then
-			collisionWithButtonPress = true
-			break
-		end
-	end
-
-	if not collisionWithButtonPress then
-		self:moveTo(goalX, goalY)
-	else
-		local found = false
-		for dx = -10, 10, 5 do
-			for dy = -10, 10, 5 do
-				local testX = goalX + dx
-				local testY = goalY + dy
-				local _, _, testCollisions, testCount = self:checkCollisions(testX, testY)
-
-				local collisionWithButtonPress = false
-				for i = 1, testCount do
-					if testCollisions[i].other:isa(ButtonPress) then
-						collisionWithButtonPress = true
-						break
-					end
-				end
-
-				if not collisionWithButtonPress then
-					self:moveTo(testX, testY)
-					found = true
-					break
-				end
+function ButtonPress:tryMoveToFreePosition(movementX, movementY)
+	-- self:moveTo(self.startPoint, self.y)	
+	local actualX, actualY, collisions, lenght = self:moveWithCollisions(movementX, movementY)
+	if lenght > 0 then
+		for index, collision in pairs(collisions) do
+			local collideObject = collision['other']
+			if collideObject:isa(ButtonPress) then
+				print("collision")
 			end
-			if found then break end
-		end
-
-		if not found then
-			self:moveTo(360, self.y)
 		end
 	end
 end
-
-
 
 function ButtonPress.getRandomButtonKey()
 	local randomIndex = math.random(1, #BUTTON_KEYS)
@@ -102,13 +68,21 @@ function ButtonPress:movementDelay(delay)
 	playdate.timer.performAfterDelay(delay, movementDelayed)
 end
 
+function ButtonPress:collisionResponse(other)
+	if other:isa(ButtonPress)then
+		return 'freeze'
+	else
+		return 'overlap'
+	end
+end
+
 function ButtonPress:update()
 	
 	if self.active == true then
-		self:moveBy(-0.5*self.bpm/3, 0)
-		-- self:moveBy(-1, 0)
+		self:tryMoveToFreePosition((self.x - ( 0.5*self.bpm/3 )), self.y)
+		
 		if self.x <= 60 then
-			self:moveTo(330, self.y)
+			self:moveTo(self.startPoint, self.y)
 			self:changeButtonSprite()
 		end
 	end
