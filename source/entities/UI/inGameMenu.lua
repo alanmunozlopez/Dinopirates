ff21d443138027f6b4596961cd24ab50813c32b4
Sebastian Zúñiga <dactrtr@gmail.com>
2025-12-10 17:50:20 +0900

    Replace item menu sprites with ingame menu image
    Removed dead-screen and titlescreen images, added ingame-menu image, and updated inGameMenu.lua to use a single menu sprite instead of individual item sprites. Item-specific logic and rendering are now commented out, focusing on the new menu image for UI display.

--- source/entities/UI/inGameMenu.lua
+++ source/entities/UI/inGameMenu.lua
@@ -1,7 +1,9 @@
 inGameMenu = {}
 class('inGameMenu').extends(Graphics.sprite)
-import "entities/UI/itemMenu"
-local shadow = Graphics.image.new(400,240) 
+-- import "entities/UI/itemMenu"
+local shadow = Graphics.image.new(400,240)
+local menuImage = Graphics.image.new('assets/images/ui/menu/ingame-menu')
+local menuSprite = nil 
 
 function inGameMenu:init()
   self.activeItem = PlayerData.activeItem 
@@ -12,8 +14,16 @@ function inGameMenu:init()
   if PlayerData.items == nil then
     
   end
-  lampItem = itemMenu("lamp",ZIndex.ui+1)
-  bootItem = itemMenu("boot",ZIndex.ui+1)
+  
+  -- Crear sprite para el menú
+  menuSprite = Graphics.sprite.new()
+  menuSprite:setImage(menuImage)
+  menuSprite:setCenter(0.5, 0.5)
+  menuSprite:moveTo(200, 120)
+  menuSprite:setZIndex(ZIndex.ui + 2)
+  
+  -- lampItem = itemMenu("lamp",ZIndex.ui+1)
+  -- bootItem = itemMenu("boot",ZIndex.ui+1)
   self:add()
 end
 
@@ -35,8 +45,11 @@ function inGameMenu:shadow()
 end
 function inGameMenu:closeMenu()
     shadow:clear(Graphics.kColorClear)
-    lampItem:remove()
-    bootItem:remove()
+    -- lampItem:remove()
+    -- bootItem:remove()
+    if menuSprite then
+        menuSprite:remove()
+    end
     print('closing menu')
     -- remove all the icons also
 end
@@ -53,10 +66,10 @@ function inGameMenu:selectItem()
     print("Item selected: " .. PlayerData.activeItem)
     -- Aquí puedes agregar la lógica específica para cada item
     if PlayerData.activeItem == 1 then
-        print("Lamp selected!")
+        -- print("Lamp selected!")
         -- Acción para la lámpara
     elseif PlayerData.activeItem == 2 then
-        print("Boot selected!")
+        -- print("Boot selected!")
         -- Acción para las botas
     end
 end
@@ -72,8 +85,11 @@ function inGameMenu:update()
       end
       if PlayerData.isEquiping == true then  
         self:shadow()
-        lampItem:show(self.playerX, self.playerY)
-        bootItem:show(64, 30)
+        if menuSprite then
+            menuSprite:add()
+        end
+        -- lampItem:show(self.playerX, self.playerY)
+        -- bootItem:show(64, 30)
         
       end
   end
